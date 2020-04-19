1535e17f6736dacdb7b03fdc384e89883976d379
TerryJIAN <kiss0817nice@gmail.com>
2020-04-19 14:47:36 +0800

    set ip
    set ip

--- app/src/main/java/com/yln/mqttdemo/Constants.java
+++ app/src/main/java/com/yln/mqttdemo/Constants.java
@@ -1,7 +1,7 @@
 package com.yln.mqttdemo;
 
 public class Constants {
-    public static final String MQTT_IP="192.168.2.111";//服务器地址
+    public static final String MQTT_IP="192.168.212.150";//服务器地址
     public static final String MQTT_PORT="1883";//服务器端口
     public static final String MQTT_SubTOPIC="Data";//订阅主题
     public static final String MQTT_pubTOPIC="Ctrl";//订阅主题
